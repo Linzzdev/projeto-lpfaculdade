ebc9317828105cd8cf2342f6273643f8d9e34dc5
Linzzdev <158218407+Linzzdev@users.noreply.github.com>
2024-06-01 00:59:52 -0300

    Att
    fixed styles, and redirect fn

---
+++ projeto-lpfaculdade-main/vite-project/src/components/Header/header.tsx
@@ -0,0 +1,27 @@
+import style from './index.module.scss'
+
+export const Header = ({ handleLinkClick }: { handleLinkClick: (event: React.MouseEvent<HTMLAnchorElement>, id: string) => void }) =>{
+    return(
+    <header  id="header" className={style.header} >
+        <ul className={style.ul}>
+            <h1>Athon.</h1>
+            <li>
+            <a href="#sectionMain" onClick={(e) => handleLinkClick(e, 'sectionMain')}>INÍCIO </a>
+            </li>
+
+            <li>
+            <a href="#sectionPj" onClick={(e) => handleLinkClick(e, 'servicesPj')}>SOBRE NÓS</a>
+            </li>
+            <li>
+            <a href="#sectionPj" onClick={(e) => handleLinkClick(e, 'servicesPj')}>CONHECIMENTOS</a>
+            </li>
+            <li>
+            <a href="#servicesPj" onClick={(e) => handleLinkClick(e, 'sectionPj')}>SERVIÇOS</a>
+            </li>
+            <li>
+            <a href="#footerContacts" onClick={(e) => handleLinkClick(e, 'footerContacts')}>CONTATO</a>
+            </li>
+        </ul>
+    </header>
+    )
+}
---
+++ projeto-lpfaculdade-main/vite-project/src/data/projects.ts
@@ -0,0 +1,33 @@
+import kHub from '../assets/logopjk.png'
+import logoppf from '../assets/logoppf.png'
+
+export const projects = [
+    {
+      img: logoppf,
+      name: "Portfólio",
+      description: "Portfólio online moderno e personalizado, desenvolvido em React para uma experiência de usuário fluida. O design exclusivo, criado com CSS Modules, reflete a identidade e o profissionalismo, proporcionando uma vitrine impactante para seus projetos e conquistas.",
+      link: "https://portfolio-linzz.vercel.app/",
+      index: 1,
+    },
+    {
+      img: kHub,
+      name: "Gerenciador Finanças",
+      description: "Gerenciador financeiro completo e intuitivo, desenvolvido sob medida em React para facilitar o controle de suas finanças. Com um design elegante e responsivo em Sass, a ferramenta permite o registro, edição, visualização e exclusão de transações (CRUD) de forma ágil e segura",
+      link: "https://react-entrega-s1-nu-kenzie-delta.vercel.app/",
+      index: 2,
+    },
+    {
+      img: logoppf,
+      name: "Portfólio",
+      description: "Portfólio online moderno e personalizado, desenvolvido em React para uma experiência de usuário fluida. O design exclusivo, criado com CSS Modules, reflete a identidade e o profissionalismo, proporcionando uma vitrine impactante para seus projetos e conquistas.",
+      link: "https://portfolio-linzz.vercel.app/",
+      index: 3,
+    },
+    {
+      img: kHub,
+      name: "Gerenciador Finanças",
+      description: "Gerenciador financeiro completo e intuitivo, desenvolvido sob medida em React para facilitar o controle de suas finanças. Com um design elegante e responsivo em Sass, a ferramenta permite o registro, edição, visualização e exclusão de transações (CRUD) de forma ágil e segura",
+      link: "https://react-entrega-s1-nu-kenzie-delta.vercel.app/",
+      index: 4,
+    },
+  ];
